f554ffaad64152e2dbf27eea2c7396d915234314
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-06-04 02:40:34 +0300

    modal-other

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuItems = document.querySelectorAll('.js-menu-item');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -18,8 +19,12 @@
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  menuItems.forEach((item) => {
+    item.addEventListener('click', toggleMenu);
+  });
+
+  // Закрытие мобильного меню на широких экранах при изменении ориентации устройства
+  window.matchMedia('(min-width: 768px)').addEventListener('change', (e) => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
